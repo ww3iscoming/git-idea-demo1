e9bb0c2b5dba5ad40f07c2100718dc0d9b0066d9
yz <957514121@qq.com>
2024-03-29 19:11:39 +0800

    我也开发

--- src/main/java/com/atguigu/Hello.java
+++ src/main/java/com/atguigu/Hello.java
@@ -6,5 +6,7 @@ public class Hello {
         System.out.println("你好1");
         System.out.println("你好2");
         System.out.println("你好2");
+        System.out.println("你好4");
+        System.out.println("你好4");
     }
 }
