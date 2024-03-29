cbd799222d32458f8b455c18db09ca382044ddd5
yz <957514121@qq.com>
2024-03-29 18:51:41 +0800

    hello1

--- src/main/java/com/atguigu/Hello.java
+++ src/main/java/com/atguigu/Hello.java
@@ -3,5 +3,7 @@ package com.atguigu;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("你好");
+        System.out.println("你好1");
+        System.out.println("你好2");
     }
 }
